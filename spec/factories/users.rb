c055e260b2ee1799df685d60b9fb2c5afe27a4b4
Alejandro Tinoco <alejandrodxwwe@hotmail.com>
2021-03-29 17:30:24 -0600

    Add new specs for user model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.unique.safe_email }
-    password { Faker::Internet.password }
+    password { Faker::Internet.password(min_length: 6) }
     password_confirmation { password } 
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,40 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  let(:user) { create(:user) }
+  let(:user) { build(:user) }
 
   describe "is invalid" do
     it "without email" do
       user.update_attribute :email, ""
       user.valid?
-      expect(user.errors[:email].to include("can't be blank"))
+      expect(user.errors[:email]).to include("can't be blank")
     end
     it "without first name" do
       user.update_attribute :first_name, nil
       user.valid?
-      expect(user.errors[:first_name].to include("can't be blank"))
+      expect(user.errors[:first_name]).to include("can't be blank")
     end
     it "without last name" do
       user.update_attribute :last_name, nil
       user.valid?
-      expect(user.errors[:last_name].to include("can't be blank"))
+      expect(user.errors[:last_name]).to include("can't be blank")
     end
-     it "without phone" do 
+    it "without phone" do 
       user.update_attribute :phone, nil
       user.valid?
-      expect(user.errors[:phone].to include("can't be blank"))
+      expect(user.errors[:phone]).to include("can't be blank")
     end
-     it "without password" do
+    it "without password" do
       user.update_attribute :password, nil
       user.valid?
-      expect(user.errors[:password].to include("can't be blank"))
-     end
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+    it "when passwords don't match" do
+      user.update_attribute :password_confirmation, Faker::Internet.password(min_length: 6)
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+    it "if the email is already taken" do
+      email = Faker::Internet.unique.safe_email 
+      user_one = create(:user, email: email)
+      user_two = build(:user, email: email)
+      user_two.valid?
+      expect(user_two.errors[:email]).to include("has already been taken")
+    end
   end
 
-  describe "is valid" do
-    it "with first name, last name, phone number, email and password" do
-      valid_user = build(:user)
-      expect(valid_user).to be_valid
-    end
+  it "is valid with first name, last name, phone number, email and password" do
+    valid_user = build(:user)
+    expect(valid_user).to be_valid
   end
 end
