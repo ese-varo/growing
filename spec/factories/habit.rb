38c427c063447895194e315ac8fb8a6d5233a589
Gregorio <gregoriovazya@gmail.com>
2021-03-29 17:08:04 -0700

    Add test for create a new habit
    Co-authored-by: jvargas98 <jvargas18@ucol.mx>

---
+++ spec/factories/habit.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :habit do
+    name { Faker::Name.name }
+    description { Faker::Lorem.paragraph }
+    start_date { Date.today }
+    end_date { Faker::Date.forward(days:35) }
+    status { false }
+  end
+end
--- spec/requests/habits_spec.rb
+++ spec/requests/habits_spec.rb
@@ -3,9 +3,35 @@ require 'rails_helper'
 RSpec.describe "Habits", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/habits/index"
+      get "/habits"
       expect(response).to have_http_status(:success)
     end
   end
 
+  describe "POST /habits" do
+    context "with valid attributes"
+      it "saves a habit in the database" do
+        expect{
+          post habits_path, params: { habit: attributes_for(:habit) }
+        }.to change(Habit, :count).by(1)
+      end
+
+      it "redirects to habit#show" do
+        post habits_path, params: { habit: attributes_for(:habit) }
+        expect(response).to redirect_to habit_path(assigns(:habit).id)
+      end
+    end
+    
+    context "with invalid attributes" do
+      it "does not save the habit in the database" do
+        expect{
+          post habits_path, params: { habit: attributes_for(:habit, name: nil) }
+        }.not_to change(Habit, :count)
+      end
+
+      it "re-renders the index template" do
+        post habits_path, params: { habit: attributes_for(:habit, name: nil) }
+        expect(response).to render_template :index
+      end
+    end
 end
