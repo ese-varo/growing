8e94819b3ce9ef5ea8357ab82f4f1a98e1e63a03
Alvaro Padilla <varoboss.ap@gmail.com>
2021-04-05 11:34:39 -0500

    Implement custom user profile page through devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_scope :user do
+    get 'profile', to: 'users/registrations#show'
+  end
 end
