a9f591650aa1acc0fc4f9b46fe6fcc407f61fc51
Alvaro Padilla <varoboss.ap@gmail.com>
2021-04-06 16:19:22 -0500

    Refactor registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,13 +1,16 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_account_update_params, only: [:update]
+  before_action :check_authentication, only: :show
+  before_action :configure_account_update_params, only: :update
 
   def show
-    redirect_to new_user_session_path, alert: "You must be logged in" unless @user = current_user
+    @user = current_user
   end
 
   protected
+
+  def check_authentication
+    redirect_to new_user_session_path, alert: "You must be logged in" unless current_user
+  end
 
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone])
