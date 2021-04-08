cce9bcf909157d913da92456b175d75b078a55be
Gregorio <gregoriovazya@gmail.com>
2021-04-08 11:48:50 -0600

    Note model with polymorphic relation

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,3 +1,4 @@
 class Day < ApplicationRecord
   belongs_to :habit
+  has_one :note, as: :noteable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_023613) do
+ActiveRecord::Schema.define(version: 2021_04_08_171556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "days", force: :cascade do |t|
-    t.boolean "status"
+    t.boolean "status", default: false
     t.text "description"
     t.date "date"
     t.bigint "habit_id", null: false
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 2021_04_08_023613) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_habits_on_user_id"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.string "description"
+    t.string "noteable_type", null: false
+    t.bigint "noteable_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["noteable_type", "noteable_id"], name: "index_notes_on_noteable"
   end
 
   create_table "users", force: :cascade do |t|
