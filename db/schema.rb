0f52d085412dbdd83b180f477dfac7a1d4bdafed
Gregorio <gregoriovazya@gmail.com>
2021-04-09 11:32:11 -0600

    Remove description field for day habit table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_171556) do
+ActiveRecord::Schema.define(version: 2021_04_09_173052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "days", force: :cascade do |t|
     t.boolean "status", default: false
-    t.text "description"
     t.date "date"
     t.bigint "habit_id", null: false
     t.datetime "created_at", precision: 6, null: false
