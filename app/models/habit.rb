60d47c16778d3173f482b4c33af61020de69da0f
Alejandro Tinoco <alejandrodxwwe@hotmail.com>
2021-04-12 14:05:26 -0500

    Add habit methods for update day habits

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,5 +1,6 @@
 class Habit < ApplicationRecord
   after_create :add_days
+  after_update :add_days, :clean_unused_days
   belongs_to :user
   has_many :days, dependent: :delete_all
   has_many :checkpoints, dependent: :delete_all
@@ -8,7 +9,7 @@ class Habit < ApplicationRecord
   def start_date_format
     self.start_date.to_s(:long) 
   end
-  
+
   def end_date_format
     self.end_date.to_s(:long) 
   end
@@ -22,13 +23,21 @@ class Habit < ApplicationRecord
     day ? day : days.first
   end
 
+  def clean_unused_days
+    day = self.end_date
+    loop do
+      self.days.where(date: day).exists? ? self.days.find_by(date: day).destroy : day += 1.day 
+      break if  day >= saved_change_to_end_date.first
+    end
+  end
+
   private
 
   def add_days
     day = start_date
-    while day <= end_date
-      self.days.create(status: false, date: day)
-      day += 1.day
+    loop do
+      self.days.where(date: day).exists? ? day += 1.day : (self.days.create(status: false, date: day) &&  day += 1.day)
+      break if day >= end_date
     end
   end
 end
