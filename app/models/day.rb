e1e0734fe6638b7882d780b2c4b9e0f1c8c06cba
Alvaro Padilla <varoboss.ap@gmail.com>
2021-04-19 23:06:28 -0500

    Add calendar identifiers

--- app/models/day.rb
+++ app/models/day.rb
@@ -3,4 +3,8 @@ class Day < ApplicationRecord
   has_one :note, as: :noteable, dependent: :destroy
   has_one :checkpoint, dependent: :destroy
   validates :date, presence: true
+
+  def before_current_date?
+    date <= Date.today
+  end
 end
